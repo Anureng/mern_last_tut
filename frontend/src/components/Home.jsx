6cd90ef0f394eccf2fb16f584cb1cf3401865b28
Anurag Nitin Sidhu <anuragsidhu72@gmail.com>
2022-10-04 21:00:43 +0530

    home_added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,12 @@
+import { Center,Heading,Text} from '@chakra-ui/react'
 import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+<Center h={"92.9vh"} w={"100vw"} bgGradient='linear(to-r, green.200, pink.500)' flexDirection={'column'}>
+<Heading fontWeight={'bold'}>Welcome!!!</Heading>
+<Text fontSize={"2xl"}>mern app</Text>
+</Center>
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,17 @@
 import React from 'react'
-import { Box, Text,Heading, HStack } from '@chakra-ui/react'
+import { Box, Text,Heading, HStack, Flex, Spacer } from '@chakra-ui/react'
 
 function Navbar() {
   return (
-    <HStack spacing={4} >
- <Feature
- title='Plan Money'
-        desc='The future can be even brighter but a goal without a plan is just a wish'
-        />
- <Feature
- title='Save Money'
- desc='You deserve good things. With a whooping 10-15% interest rate per annum,  grow your savings on your own terms with our completely automated process'
- />
-    </HStack>
+    <Flex gap={"3rem"} bgGradient='linear(to-r, green.200, pink.500)' p={2}>
+      <Text fontSize={"2xl"}>Anurag</Text>
+      <Spacer></Spacer>
+      <Text fontSize={"2xl"}>Home</Text>
+      <Text fontSize={"2xl"}>About</Text>
+      <Text fontSize={"2xl"} >Contact</Text>
+      <Text fontSize={"2xl"}>Registration</Text>
+      <Text fontSize={"2xl"} mr="1rem">Login</Text>
+    </Flex>
   )
 }
-function Feature({title,desc,...rest}){
-    return(
-    <Box p={5} borderWidth='1px' {...rest} >
-    
-        <Heading>{title}</Heading>
-        <Text>{desc}</Text>
-    </Box>
-    )
-    }
 export default Navbar
